69b633ce4a3ee5a240e8956aadb67a58adca4146
zavden <davz95@hotmail.com>
2022-12-28 17:10:21 -0600

    first commit

--- animathed/animathed.py
+++ animathed/animathed.py
@@ -4,11 +4,11 @@ import os
 import platform
 
 
-def make_frame(draw_frame_func, frame_num, width, height):
+def make_frame(draw_frame_func, t, width, height):
     """
-    Convert Cairo.ImageSurface to RAW data
+    Convert cairo.ImageSurface to RAW (bytes) data
     """
-    frame = draw_frame_func(frame_num, width, height)
+    frame = draw_frame_func(t, width, height)
     data = frame.get_data().tobytes()
     return data
 
@@ -16,49 +16,48 @@ def open_file(file_path):
     """
     Open video file at the end
     """
-        current_os = platform.system()
+    current_os = platform.system()
 
-        if current_os == "Windows":
-            os.startfile(file_path)
-        else:
-            commands = []
-            if current_os == "Linux":
-                commands.append("xdg-open")
-            elif current_os.startswith("CYGWIN"):
-                commands.append("cygstart")
-            else:  # Assume macOS
-                commands.append("open")
+    if current_os == "Windows":
+        os.startfile(file_path)
+    else:
+        commands = []
+        if current_os == "Linux":
+            commands.append("xdg-open")
+        elif current_os.startswith("CYGWIN"):
+            commands.append("cygstart")
+        else:  # Assume macOS
+            commands.append("open")
 
-            commands.append(file_path)
+        commands.append(file_path)
 
-            FNULL = open(os.devnull, 'w')
-            sp.call(commands, stdout=FNULL, stderr=sp.STDOUT)
-            FNULL.close()
+        sp.call(commands, stderr=sp.STDOUT)
 
 
-def run(draw_frame_func, frame_count, output_name, width=500, height=500, frame_rate=30):
-    # frame_count += 1
+def run(draw_frame_func, total_frames, output_name, width=500, height=500, frame_rate=30):
+    # total_frames += 1
     command = [
         'ffmpeg',
-        '-y',
+        '-y', # replace the video if it already exists
         '-f',
-        'rawvideo',
+        'rawvideo', # input data: bytes
         '-s', '%dx%d' % (width, height),
         '-pix_fmt', 'rgba',
         '-r', str(frame_rate),
-        '-i', '-',
+        '-i',
+        '-', # pipe input
         '-loglevel','error',
         '-vcodec', 'libx264', '-pix_fmt', 'yuv420p',
-        f'videos/{output_name}.mp4'
-    ] 
+        f'videos/{output_name}.mp4' # output
+    ]
 
     p = sp.Popen(command, stdin=sp.PIPE)
-    bar = progressbar.ProgressBar(maxval=frame_count).start()
+    bar = progressbar.ProgressBar(maxval=total_frames).start()
 
-    for frame_num in range(frame_count):
-        data = make_frame(draw_frame_func, frame_num, width, height)
+    for t in range(total_frames):
+        data = make_frame(draw_frame_func, t, width, height)
         p.stdin.write(data) # add image raw to the buffer
-        bar.update(frame_num)
+        bar.update(t)
     p.stdin.close()
     p.wait()
     open_file(f"videos/{output_name}.mp4")
